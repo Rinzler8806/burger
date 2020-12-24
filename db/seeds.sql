c32ad9e79d5f260b06a9cb87e7c06dff99639060
Rinzler8806 <shane.rogers916@gmail.com>
2020-12-23 18:25:40 -0800

    initial commit

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers (name)
+VALUES ("Double Cheeseburger");
+
+INSERT INTO burgers (name)
+VALUES ("Ultimate Cheeseburger");
+
+INSERT INTO burgers (name)
+VALUES ("Mushroom Cheeseburger");
